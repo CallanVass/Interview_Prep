e8da98e182f37877ed049257a5584a71939d00b6
Callan <callan.v@fake.com>
2024-04-19 11:37:11 +1000

    Played with Fetch. Must do more research.

---
+++ jsFetch.js
@@ -0,0 +1,2 @@
+// Research Fetch and try it out
+
--- jsPractice.js
+++ jsPractice.js
@@ -15,14 +15,26 @@ function fizzBuzz() {
     }
 }
 
-fizzBuzz()
+// fizzBuzz()
 
 // FizzBuzz with Array
 
-function fizzBuzz(n) {
+function fizzBuzzWithArray(n) {
     let result = []
 
     for (let i = 1; i <= n; i++) {
-
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz")
+        } else if (i % 3 === 0) {
+            result.push("Fizz")
+        } else if (i % 5 === 0) {
+            result.push("Buzz")
+        } else {
+            result.push(i.toString())
+        }
     }
+    console.log(result)
+    return result
 }
+
+fizzBuzzWithArray(100)
